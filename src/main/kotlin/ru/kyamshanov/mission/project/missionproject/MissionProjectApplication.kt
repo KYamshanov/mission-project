acb0d7d2c79a1071b39030f80129c4e998cee5db
Ямшанов Константин Алексеевич (4059153) <yamshanov@dev.vtb.ru>
2023-01-21 13:50:55 +0400

    Initial commit

---
+++ src/main/kotlin/ru/kyamshanov/mission/project/missionproject/MissionProjectApplication.kt
@@ -0,0 +1,11 @@
+package ru.kyamshanov.mission.project.missionproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MissionProjectApplication
+
+fun main(args: Array<String>) {
+    runApplication<MissionProjectApplication>(*args)
+}
---
+++ src/test/kotlin/ru/kyamshanov/mission/project/missionproject/MissionProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.kyamshanov.mission.project.missionproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MissionProjectApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
